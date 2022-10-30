5fa0bf2705770a8817e7a0c85b50c26577130cf6
nitwhiz <schonfeldandy@gmail.com>
2022-10-30 16:24:59 +0100

    add deathmatch bedrock target randomizer

--- pkg/room/room_bedrock.go
+++ pkg/room/room_bedrock.go
@@ -99,33 +99,10 @@ func (d *BedrockDistribution) Randomize() {
 	defer d.mu.Unlock()
 	d.mu.Lock()
 
-	d.targetMap = map[string]string{}
-
-	d.randomGameIdBag.NextBag()
-
-	bSize := d.randomGameIdBag.GetSize()
-
-	if bSize <= 1 {
-		return
-	}
-
-	for i := 0; i < bSize; i++ {
-		randomId := d.randomGameIdBag.NextElement()
-
-		d.targetMap[randomId] = randomId
-	}
-
-	// just to be extra sure: new bag
-	d.randomGameIdBag.NextBag()
-
-	for id := range d.targetMap {
-		target := d.randomGameIdBag.NextElement()
-
-		if d.random.Probably(.25) {
-			continue
-		}
-
-		d.targetMap[id] = target
+	if d.room.GetRunningGamesCount() > 2 {
+		d.defaultRandomizer()
+	} else {
+		d.deathMatchRandomizer()
 	}
 
 	d.bus.Publish(&event.Event{
